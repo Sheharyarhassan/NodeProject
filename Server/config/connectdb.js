d932f669a71ed193080a309aa378ce24d60efb30
Sheharyarhassan <shehryar@techtonex.com>
2025-10-02 17:02:37 +0500

    bcrypt

--- Server/config/connectdb.js
+++ Server/config/connectdb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcrypt');
 const { userType, Signup } = require("../models/userModels");
 require("dotenv").config();
 

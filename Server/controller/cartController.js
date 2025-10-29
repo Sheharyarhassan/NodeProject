e4206da4f566e5b70e1a51999bed471e999a89eb
Sheharyarhassan <shehryar@techtonex.com>
2025-10-29 18:52:55 +0500

    Cart Removal after completion

--- Server/controller/cartController.js
+++ Server/controller/cartController.js
@@ -100,6 +100,7 @@ const checkOut = async (req, res) => {
         });
       }
     }
+    await Cart.findByIdAndDelete(cart._id)
     res.status(200).json({message: 'Your order is Confirmed'});
   } catch (err) {
     res.status(500).json({message: err})
